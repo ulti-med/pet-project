742bfb6aaf76aeb0df9eadb3a9bffaed8dab9e9e
Filipp Polonskiy <filipp.polonskiy@synapx.co.uk>
2024-05-07 19:42:04 +0100

    feat: finish random character page

---
+++ random.js
@@ -0,0 +1,35 @@
+const name = document.querySelector("#character-name");
+const image = document.querySelector("#character-image");
+const button = document.querySelector("#fetch-character");
+const addFavouriteButton = document.querySelector("#add-to-favourite");
+let id;
+
+const getCharacter = async () => {
+  id = Math.floor(Math.random() * 826) + 1;
+  const url = `https://rickandmortyapi.com/api/character/${id}`;
+  const data = await fetch(url);
+  const parsedData = await data.json();
+  name.textContent = parsedData.name;
+  image.src = parsedData.image;
+};
+
+const addToFavourite = async () => {
+  const localCharacters = localStorage.getItem("characters");
+  if (localCharacters) {
+    const parsedCharacters = JSON.parse(localCharacters);
+    if (parsedCharacters.includes(id)) {
+      return;
+    }
+    localStorage.setItem(
+      "characters",
+      JSON.stringify([...parsedCharacters, id])
+    );
+  } else {
+    localStorage.setItem("characters", JSON.stringify([id]));
+  }
+};
+
+getCharacter();
+
+button.onclick = getCharacter;
+addFavouriteButton.onclick = addToFavourite;
